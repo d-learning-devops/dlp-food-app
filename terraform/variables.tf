95a3ad63f8a140810242472c2a1374f7b8e90670
DLP <71308926+d-learning-devops@users.noreply.github.com>
2026-08-29 21:09:51 +0530

    Add Terraform variables for resource management
    Define variables for resource configuration in Terraform.

---
+++ terraform/variables.tf
@@ -0,0 +1,23 @@
+variable "prefix" {
+  type        = string
+  default     = "zomatoclone"
+  description = "Prefix for all resources"
+}
+
+variable "location" {
+  type        = string
+  default     = "eastus"
+  description = "Azure region"
+}
+
+variable "node_count" {
+  type        = number
+  default     = 2
+  description = "Initial node pool count"
+}
+
+variable "vm_size" {
+  type        = string
+  default     = "Standard_B2s"
+  description = "VM SKU for the worker nodes"
+}
